1fb80fe1a57625e5136c693cdabb5e29ae4a0d67
MuhammedYaseenediflex <96539605+MuhammedYaseenediflex@users.noreply.github.com>
2021-12-23 16:58:07 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,301 +1,107 @@
-class script(object):
-    START_TXT = """𝙷𝙴𝙻𝙾 {},
-𝙼𝚢 𝚗𝚊𝚖𝚎 , <a href='https://t.me/Dqautofl_bot'>𝐀𝐍𝐍𝐀 𝐁𝐄𝐍</a>, 𝙸𝚝'𝚜 𝚟𝚎𝚛𝚢 𝚎𝚊𝚜𝚢 𝚓𝚞𝚜𝚝 𝚊𝚍𝚍 𝚖𝚎 𝚝𝚘 𝚢𝚘𝚞𝚛 𝚐𝚛𝚘𝚞𝚙 𝚊𝚗𝚍 𝚖𝚊𝚔𝚎 𝚖𝚎 𝚊𝚍𝚖𝚒𝚗, 𝚃𝚑𝚊𝚝𝚜 𝚊𝚕𝚕 𝙸'𝚕𝚕 𝚙𝚛𝚘𝚟𝚒𝚍𝚎 𝚖𝚘𝚟𝚒𝚎𝚜 𝚝𝚑𝚎𝚛𝚎 🤓
-"""
-    HELP_TXT = """𝙷𝙴𝚈 {}
-𝘏𝘦𝘳𝘦 𝘐𝘴 𝘛𝘩𝘦 𝘏𝘦𝘭𝘱 𝘍𝘰𝘳 𝘔𝘺 𝘊𝘰𝘮𝘮𝘢𝘯𝘥𝘴."""
-    ABOUT_TXT = """
-𝗧𝗛𝗜𝗦 𝗔𝗡𝗡𝗔 𝗕𝗘𝗡'𝗦 𝗔𝗕𝗢𝗨𝗧 𝗠𝗦𝗚
-✵✵✵✵✵✵✵✵✵✵✵✵✵✵✵✵✵✵✵✵
-╔════❰ ꪖ᥇ꪮꪊ𝓽 ꪑ𝘴ᧁ ❱═❍⊱❁۪۪
-║╭━━━━━━━━━━━━━━━➣ 
-║┣⪼ 𝙈𝙔 𝙉𝘼𝙈𝙀 - <a href="https://t.me/Dqautofl_Bot"> 𝑨𝑵𝑵𝑨 𝑩𝑬𝑵 </a>
-║┣⪼ Ⓓ︎Ⓔ︎Ⓥ︎1 - <a href="https://t.me/pro_editor_tg"> Lalluᵗᵍ </a>
-║┣⪼ Ⓓ︎Ⓔ︎Ⓥ︎2 - <a href="https://t.me/PANDITHAN_SIR"> ⸙ꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋM-STER </a>
-║┣⪼ 𝓛𝓲𝓫𝓻𝓪𝓻𝓻𝔂 - 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
-║┣⪼ 𝓛𝓪𝓷𝓰𝓾𝓪𝓰𝓮 - 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
-║┣⪼ 𝓓𝓪𝓽𝓪 𝓑𝓪𝓼𝓮 - 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
-║┣⪼ 𝓑𝓸𝓽 𝓼𝓮𝓻𝓿𝓮𝓻 -  𝙷𝙴𝚁𝙾𝙺𝚄
-║┣⪼ 𝓑𝓾𝓲𝓵𝓭 𝓢𝓽𝓪𝓽𝓾𝓼 - v1.0.1 [ 𝙱𝙴𝚃𝙰 ]
-║╰━━━━━━━━━━━━━━━➣ ╚══════════════════❍⊱❁۪۪"""
-    SOURCE_TXT = """<b>NOTE:</b>
-- 𝑰 𝒂𝒎  𝒂 𝒐𝒑𝒆𝒏 𝒔𝒐𝒖𝒓𝒄𝒆 𝒑𝒓𝒐𝒋𝒆𝒄𝒕. 
-- ՏOᑌᖇᑕᗴ ᑕOᗪᗴ - <a href="https://github.com/Lallu-lallus/AnnaBen_robot"> 𝗖𝗟𝗜𝗖𝗞 𝗛𝗘𝗥𝗘 </a>
-
-𝗠𝗔𝗦𝗧𝗘𝗥:
-<a href="https://t.me/team_annaben"> 𝑻𝑬𝑨𝑴 𝑨𝑵𝑵𝑨 </a>"""
-    WHOIS_TXT ="""<b>WHOIS MODULE</b>
-Note:- Give a user details
-
-•/whois :-give a user full details"""
-    ALIVE_TXT ="""<b>ALIVE MODULE</b>
-• /alive - check me alive or dead🤧
-Just for a rasam😂"""
-    CORONA_TXT ="""<b>Here is the help for the coron information module</b>
-➡️ /covid <code>(countryname)</code> <b>you can find a corona information of every country</b>
-
-➡️ <b>example</b> : - /covid India"""
-    STICKER_TXT ="""<b>COMMAND /stickerid\n𝖨𝖿 𝖸𝗈𝗎 𝖭𝖾𝖾𝖽 𝖳𝖾𝗅𝖾𝗀𝗋𝖺𝗆 𝖲𝗍𝗂𝖼𝗄𝖾𝗋 𝖨𝖽 𝖢𝗅𝗂𝖼𝗄 /stickerid 𝖳𝗈 𝖦𝖾𝗍 𝖲𝗍𝗂𝖼𝗄𝖾𝗋 𝖨𝖽 (𝖱𝖾𝗉𝗅𝗒 𝖶𝗂𝗍𝗁 𝖲𝗍𝗂𝖼𝗄𝖾𝗋)</b>"""
-    SONG_TXT ="""<b>SONG MODULE</b>
-Song Download
-Song Download Module, For Those Who Love Music
-
-🎈 Command
-
-• /song or /mp3 (songname) - download song from yt servers.
-• /video or /mp4 (songname) - download video from yt servers
-
-Usage
-- working pm and groups"""
-    JSON_TXT ="""<b>JSON MODULE</b>
-JSON:
-Bot returns json for all replied messages with /json
-
-Features:
-Message Editting JSON
-Pm Support
-Group Support
-
-Note:
-Everyone can use this command , if spaming happens bot will automatically ban you from the group"""
-    PIN_TXT ="""<b>PIN MODULE</b>
-
-<b>Pin :</b>
-
-<b>All The Pin Related Commands Can Be Found Here; Keep Your Chat Up To Date On The Latest News With A Simple Pinned Message!</b>
-
-<b>📚 Commands & Usage:</b>
-
-◉ /Pin :- Pin The Message You Replied To Message To Send A Notification To Group Members
-
-◉ /Unpin :- Unpin The Current Pinned Message. If Used As A Reply, Unpins The Replied To Message"""
-    FUN_TXT ="""<b>FUN MODULE</b> 
-    
-<b>🎲 NOTHING MUCH JUST SOME FUN THINGS</b>
-t𝗋𝗒 𝗍𝗁𝗂𝗌 𝖮𝗎𝗍: 
-𝟣. /dice - Roll The Dice 
-𝟤. /Throw 𝗈𝗋 /Dart - 𝖳𝗈 𝖬𝖺𝗄𝖾 Drat 
-3. /Runs - Jokes 
-4. /Goal or /Shoot - To Make A Goal Or Shoot
-5. /luck or /cownd - Spin the Lucky"""
-    MANUALFILTER_TXT = """Help: <b>Filters</b>
-
-- Filter is the feature were users can set automated replies for a particular keyword and Dingdi will respond whenever a keyword is found the message
-
-<b>NOTE:</b>
-1. IMDb should have admin privillage.
-2. Only admins can add filters in a chat.
-3. Alert buttons have a limit of 64 characters.
-
-<b>Commands and Usage:</b>
-• /filter - add a filter in chat.
-• /filters - list all the filters of a chat.
-• /del - delete a specific filter in chat.
-• /delall - delete the whole filters in a chat (chat owner only)."""
-
-    BUTTON_TXT = """Help: <b>Buttons</b>
-
-- tgmoviebot support both url and alert inline buttons.
-
-<b>NOTE:</b>
-1. Telegram will not allows you to send buttons without any content, so content is mandatory.
-2. IMDb supports buttons with any telegram media type.
-3. Buttons should be properly parsed as markdown format.
-
-<b>URL buttons:</b>
-<code>[Button Text](buttonurl:https://t.me/josprojects)</code>
-
-<b>Alert buttons:</b>
-<code>[Button Text](buttonalert:This is an alert message)</code>"""
-
-    AUTOFILTER_TXT = """Help: <b>Auto Filter</b>
-
-<b>NOTE:</b>
-1. Make me the admin of your channel if it's private.
-2. Make sure that your channel does not contains camrips, porn and fake files.
-3. Forward the last message to me with quotes.
- I'll add all the files in that channel to my db."""
-
-    CONNECTION_TXT = """Help: <b>Connections</b>
-
-- Used to connect bot to PM for managing filters 
-- It helps to avoid spamming in groups.
-
-<b>NOTE:</b>
-1. Only admins can add a connection.
-2. Send <code>/connect</code> for connecting me to ur PM
-
-<b>Commands and Usage:</b>
-• /connect  - connect a particular chat to your PM.
-• /disconnect  - disconnect from a chat.
-• /connections - list all your connections."""
-
-    AUTO_MANUAL_TXT = """Help: <b>Filters</b>
-
-<b>Select a filters type Below:</b>"""
-
-    PASTE_TXT = """Help: <b>Paste</b>
-
-Paste some texts or documents on a website!
-
-<b>Commands and Usage:</b>
-• /paste [text] - paste the given text on Pasty
-• /paste [reply] - paste the replied text on Pasty
-
-<b>NOTE:</b>
-• IMDb should have admin privillage.
-• These commands works on both pm and group.
-• These commands can be used by any group member."""
-
-    TGRAPH_TXT = """Help: <b>TGraph & Paste</b>
-
-Do as you wish with telegra.ph module!
-
-<b>Commands and Usage:</b>
-• /tgmedia or /tgraph - upload supported media (within 5MB) to telegraph.
-
-<b>NOTE:</b>
-• IMDb should have admin privillage.
-• These commands works on both pm and group.
-• These commands can be used by any group member."""
-
-    INFO_TXT = """Help: <b>Information</b>
-
-Get information about something!
-
-<b>Commands and Usage:</b>
-• /id - get id of a specifed user.
-• /info  - get information about a user.
-• /json - get the json details of a message.
-
-<b>NOTE:</b>
-• IMDb should have admin privillage.
-• These commands works on both pm and group.
-• These commands can be used by any group member."""
-
-    GTRANS_TXT = """Help: <b>Google Translator</b>
-
-Translate texts to a specific language!
-
-<b>Commands and Usage:</b>
-• /tr [language code][reply] - translate replied message to specific language.
-
-<b>NOTE:</b>
-• IMDb should have admin privillage.
-• These commands works on both pm and group.
-• IMDb can translate texts to 200+ languages."""
-
-    SEARCH_TXT = """Help: <b>IMDb</b>
-
-Search many things without leaving telegram!
-
-<b>Commands and Usage:</b>
-• /imdb  - get the film information from IMDb source.
-• /search  - get the film information from various sources.
-
-<b>NOTE:</b>
-• IMDb should have admin privillage.
-• More search tools can be found on inline.
-• Those commands works on both pm and group."""
-
-    PURGE_TXT = """Help: <b>Purge</b>
-
-Need to delete lots of messages? That's what purges are for!
-
-<b>Commands and Usage:</b>
-• /purge - delete all messages from the replied to message, to the current message.
-
-<b>NOTE:</b>
-• IMDb should have admin privillage.
-• These commands works on group.
-• These commands can be used by Only admin."""
-
-    RESTRIC_TXT = """Help: <b>Restrictions</b>
-
-Some people need to be publicly banned; spammers, annoyances, or just trolls.
-
-This module allows you to do that easily, by exposing some common actions, so everyone will see!
-
-<b>Commands and Usage:</b>
-• /ban - ban a user.
-• /tban - temporarily ban a user. Example time values: 4m = 4 minutes, 3h = 3 hours, 6d = 6 days, 5w = 5 weeks.
-• /mute - mute a user.
-• /tmute - temporarily mute a user. Example time values: 4m = 4 minutes, 3h = 3 hours, 6d = 6 days, 5w = 5 weeks.
-• /unban or /unmute - unmute a user & unban a user.
-
-<b>Examples:</b>
-- Mute a user for two hours.
--> <code>/tmute @username 2h</code>
-
-<b>NOTE:</b>
-• IMDb should have admin privillage.
-• These commands works on group.
-• These commands can be used by Only admin."""
-
-    ADMIN_TXT = """Help: <b>Admin Mods</b>
-
-<b>NOTE:</b>
-This module only works for my admins
-
-<b>Commands and Usage:</b>
-• /logs - to get the rescent errors.
-• /stats - to get status of files in db.
-• /delete - to delete a specific file from db.
-• /users - to get list of my users and ids.
-• /chats - to get list of the my chats and ids.
-• /leave - to leave from a chat.
-• /disable - do disable a chat.
-• /ban_users - to ban a user.
-• /unban_users - to unban a user.
-• /channel - to get list of total connected channels.
-• /broadcast - to broadcast a message to all users."""
-
-    STATUS_TXT = """<b>Total Files:</b> <code>{}</code>
-<b>Total Users:</b> <code>{}</code>
-<b>Total Chats:</b> <code>{}</code>
-<b>Used Storage:</b> <code>{}</code> MiB
-<b>Free Storage:</b> <code>{}</code> MiB"""
-
-    FORCESUB_TXT = """**♦️ READ THIS INSTRUCTION ♦️**
-
-__🗣 In Order To Get The Movie Requested By You in Our Groups, You Will Have To Join Our Official Channel First. After That, Try Accessing That Movie Again From Our Group. I'll Send You That Movie Privately 🙈__
-
-**👇 JOIN THIS CHANNEL & TRY AGAIN 👇**"""
-
-    LOG_TEXT_G = """#NewGroup
-Group = {}(<code>{}</code>)
-Total Members = <code>{}</code>
-Added By - {}
-"""
-
-    LOG_TEXT_P = """#NewUser
-ID - <code>{}</code>
-Name - {}"""
-
-    ZOMBIES_TXT = """Help: <b>Zombies</b>
-
-<b>Kick incative members from group. Add me as admin with ban users permission in group.</b>
-
-<b>Commands and Usage:</b>
-• /inkick - command with required arguments and i will kick members from group.
-• /instatus - to check current status of chat member from group.
-• /inkick within_month long_time_ago - to kick users who are offline for more than 6-7 days.
-• /inkick long_time_ago - to kick members who are offline for more than a month and Deleted Accounts.
-• /dkick - to kick deleted accounts."""
-
-    CREATOR_REQUIRED = """❗You have to be the group creator to do that."""
-      
-    INPUT_REQUIRED = "❗ **Arguments Required**"
-      
-    KICKED = """✔️ Successfully Kicked {} members according to the arguments provided."""
-      
-    START_KICK = """🚮 Removing inactive members this may take a while..."""
-      
-    ADMIN_REQUIRED = """❗I am not an admin here\n__Leaving this chat, add me again as admin with ban user permission."""
-      
-    DKICK = """✔️ Kicked {} Deleted Accounts Successfully."""
-      
-    FETCHING_INFO = """Collecting users information..."""
-      
-    STATUS = """{}\nChat Member Status**\n\n```recently``` - {}\n```within_week``` - {}\n```within_month``` - {}\n```long_time_ago``` - {}\nDeleted Account - {}\nBot - {}\nUnCached - {}
-"""
+​class​ ​script​(​object​): 
+ ​    ​START_TXT​ ​=​ ​"""𝙷𝙴𝙻𝙾 {}, 
+ ​𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href=https://t.me/movie_bus6{}>{}</a>, 𝙸 𝙲𝙰𝙽 𝙿𝚁𝙾𝚅𝙸𝙳𝙴 𝙼𝙾𝚅𝙸𝙴𝚂, 𝙹𝚄𝚂𝚃 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 𝙰𝙽𝙳 𝙴𝙽𝙹𝙾𝚈 😍""" 
+ ​    ​HELP_TXT​ ​=​ ​"""𝙷𝙴𝚈 {} 
+ ​𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂.""" 
+ ​    ​ABOUT_TXT​ ​=​ ​"""✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {} 
+ ​✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/Mallu_mass>ℤ𝕚𝕟𝕒𝕟 𝕋𝕖𝕔𝕙2.0</a> 
+ ​✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼 
+ ​✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹 
+ ​✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱 
+ ​✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: 𝙷𝙴𝚁𝙾𝙺𝚄 
+ ​✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v6.8.1 [ 𝙴𝙳𝙸𝙵𝙻𝙴𝚇 ]""" 
+ ​    ​SOURCE_TXT​ ​=​ ​"""<b>NOTE:</b> 
+ ​- 𝚁𝚎𝚡𝚎𝚛 is a open source project.  
+ ​- Source - https://github.com/Ediflex/AM-Collab-Ediflex-bot
+  
+ ​<b>DEVS:</b> 
+ ​- <a href=https://t.me/movie_bus6> 𝗥𝗲𝘅𝗲𝗿📽™ </a>""" 
+ ​    ​MANUELFILTER_TXT​ ​=​ ​"""Help: <b>Filters</b> 
+  
+ ​- Filter is the feature were users can set automated replies for a particular keyword and Movieworld will respond whenever a keyword is found the message 
+  
+ ​<b>NOTE:</b> 
+ ​1. Rexer should have admin privillage. 
+ ​2. only admins can add filters in a chat. 
+ ​3. alert buttons have a limit of 64 characters. 
+  
+ ​<b>Commands and Usage:</b> 
+ ​• /filter - <code>add a filter in chat</code> 
+ ​• /filters - <code>list all the filters of a chat</code> 
+ ​• /del - <code>delete a specific filter in chat</code> 
+ ​• /delall - <code>delete the whole filters in a chat (chat owner only)</code>""" 
+ ​    ​BUTTON_TXT​ ​=​ ​"""Help: <b>Buttons</b> 
+  
+ ​- Rexer Supports both url and alert inline buttons. 
+  
+ ​<b>NOTE:</b> 
+ ​1. Telegram will not allows you to send buttons without any content, so content is mandatory. 
+ ​2. Rexer supports buttons with any telegram media type. 
+ ​3. Buttons should be properly parsed as markdown format 
+  
+ ​<b>URL buttons:</b> 
+ ​<code>[Button Text](buttonurl:https://t.me/movie_bus6)</code> 
+  
+ ​<b>Alert buttons:</b> 
+ ​<code>[Button Text](buttonalert:This is an alert message)</code>""" 
+ ​    ​AUTOFILTER_TXT​ ​=​ ​"""Help: <b>Auto Filter</b> 
+  
+ ​<b>NOTE:</b> 
+ ​1. Make me the admin of your channel if it's private. 
+ ​2. make sure that your channel does not contains camrips, porn and fake files. 
+ ​3. Forward the last message to me with quotes. 
+ ​ I'll add all the files in that channel to my db.""" 
+ ​    ​CONNECTION_TXT​ ​=​ ​"""Help: <b>Connections</b> 
+  
+ ​- Used to connect bot to PM for managing filters  
+ ​- it helps to avoid spamming in groups. 
+  
+ ​<b>NOTE:</b> 
+ ​1. Only admins can add a connection. 
+ ​2. Send <code>/connect</code> for connecting me to ur PM 
+  
+ ​<b>Commands and Usage:</b> 
+ ​• /connect  - <code>connect a particular chat to your PM</code> 
+ ​• /disconnect  - <code>disconnect from a chat</code> 
+ ​• /connections - <code>list all your connections</code>""" 
+ ​    ​EXTRAMOD_TXT​ ​=​ ​"""Help: <b>Extra Modules</b> 
+  
+ ​<b>NOTE:</b> 
+ ​these are the extra features of Movie World 
+  
+ ​<b>Commands and Usage:</b> 
+ ​• /id - <code>get id of a specifed user.</code> 
+ ​• /info  - <code>get information about a user.</code> 
+ ​• /imdb  - <code>get the film information from IMDb source.</code> 
+ ​• /search  - <code>get the film information from various sources.</code>""" 
+ ​    ​ADMIN_TXT​ ​=​ ​"""Help: <b>Admin mods</b> 
+  
+ ​<b>NOTE:</b> 
+ ​This module only works for my admins 
+  
+ ​<b>Commands and Usage:</b> 
+ ​• /logs - <code>to get the rescent errors</code> 
+ ​• /stats - <code>to get status of files in db.</code> 
+ ​• /delete - <code>to delete a specific file from db.</code> 
+ ​• /users - <code>to get list of my users and ids.</code> 
+ ​• /chats - <code>to get list of the my chats and ids </code> 
+ ​• /leave  - <code>to leave from a chat.</code> 
+ ​• /disable  -  <code>do disable a chat.</code> 
+ ​• /ban  - <code>to ban a user.</code> 
+ ​• /unban  - <code>to unban a user.</code> 
+ ​• /channel - <code>to get list of total connected channels</code> 
+ ​• /broadcast - <code>to broadcast a message to all users</code>""" 
+ ​    ​STATUS_TXT​ ​=​ ​"""★ 𝚃𝙾𝚃𝙰𝙻 𝙵𝙸𝙻𝙴𝚂: <code>{}</code> 
+ ​★ 𝚃𝙾𝚃𝙰𝙻 𝚄𝚂𝙴𝚁𝚂: <code>{}</code> 
+ ​★ 𝚃𝙾𝚃𝙰𝙻 𝙲𝙷𝙰𝚃𝚂: <code>{}</code> 
+ ​★ 𝚄𝚂𝙴𝙳 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝙱 
+ ​★ 𝙵𝚁𝙴𝙴 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝙱""" 
+ ​    ​LOG_TEXT_G​ ​=​ ​"""#NewGroup 
+ ​Group = {}(<code>{}</code>) 
+ ​Total Members = <code>{}</code> 
+ ​Added By - {} 
+ ​""" 
+ ​    ​LOG_TEXT_P​ ​=​ ​"""#NewUser 
+ ​ID - <code>{}</code> 
+ ​Name - {} 
+ ​"""
